d38d18597ed47316b97f2210ff83ad9f8cc3d0e4
LiuHX01 <2539807128@qq.com>
2023-02-19 09:09:39 +0800

    feat: Yslider选定范围，TODO解决反向

--- src/components/MotionRugs.vue
+++ src/components/MotionRugs.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted, reactive, ref } from "vue";
 import { MotionAdaptor } from "./Adaptor";
-import { FRAME_LENGTH } from "./Constants.js";
+import { FRAME_LENGTH, FILE_COUNT } from "./Constants.js";
 import { myWorker } from "./MyWorker.js";
 
 const emit = defineEmits(["changeRange", "fullScreenChange"]);
@@ -10,6 +10,7 @@ const load = reactive({
     loading: true,
 });
 const value1 = ref([0, 50]);
+const valueY = ref([FILE_COUNT, 1]);
 
 const canvasItem = {
     canvas: null,
@@ -21,7 +22,7 @@ const pixelHighlightValue = ref(false);
 
 const pixelHighlightChange = (value) => {
     if (value) {
-        drawMask(value1.value[0], value1.value[1]);
+        drawMask(value1.value[0], value1.value[1], valueY.value[0], valueY.value[1]);
     } else {
         if (canvasItem.ctx) {
             canvasItem.ctx.clearRect(0, 0, canvasItem.canvas.width, canvasItem.canvas.height);
@@ -34,11 +35,18 @@ const pixelHighlightChange = (value) => {
 const changeRange = (range) => {
     emit("changeRange", range);
     if (pixelHighlightValue.value) {
-        drawMask(range[0], range[1]);
+        drawMask(range[0], range[1], valueY.value[0], valueY.value[1]);
     }
 };
 
-const drawMask = (start, end) => {
+const changeRangeY = (range) => {
+    // emit("changeRangeY", range);
+    if (pixelHighlightValue.value) {
+        drawMask(value1.value[0], value1.value[1], range[0], range[1]);
+    }
+};
+
+const drawMask = (start, end, startY, endY) => {
     if (canvasItem.ctx) {
         if (canvasItem.highlight) {
             canvasItem.ctx.clearRect(0, 0, canvasItem.canvas.width, canvasItem.canvas.height);
@@ -59,6 +67,20 @@ const drawMask = (start, end) => {
         canvasItem.ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
         canvasItem.ctx.fillRect(backX, backY, backWidth, backHeight);
 
+        const topX = start;
+        const topY = 0;
+        const topWidth = end - start;
+        const topHeight = startY;
+        canvasItem.ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        canvasItem.ctx.fillRect(topX, topY, topWidth, topHeight);
+
+        const bottomX = start;
+        const bottomY = endY;
+        const bottomWidth = end - start;
+        const bottomHeight = canvasItem.canvas.height - endY;
+        canvasItem.ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+        canvasItem.ctx.fillRect(bottomX, bottomY, bottomWidth, bottomHeight);
+
         canvasItem.highlight = true;
     }
 };
@@ -76,7 +98,7 @@ const changeStrategy = (strategyName) => {
             canvasItem.ctx.clearRect(0, 0, canvasItem.canvas.width, canvasItem.canvas.height);
             canvasItem.ctx.putImageData(strategyImgs[strategyName], 0, 0);
             if (pixelHighlightValue.value) {
-                drawMask(value1.value[0], value1.value[1]);
+                drawMask(value1.value[0], value1.value[1], valueY.value[0], valueY.value[1]);
             }
         } else {
             console.log("no such strategy");
@@ -119,6 +141,19 @@ onMounted(() => {
 
 <template>
     <div id="motionrug-container">
+        <div class="slider-y-container">
+            <el-slider
+                class="slider-y"
+                v-model="valueY"
+                range
+                :max="FILE_COUNT"
+                :min="1"
+                :step="-1"
+                vertical
+                height="90px"
+                @change="changeRangeY"
+            />
+        </div>
         <el-scrollbar v-loading="load.loading" element-loading-background="rgba(235,235,235,1)">
             <canvas id="canvas"></canvas>
         </el-scrollbar>
@@ -161,6 +196,10 @@ onMounted(() => {
 </template>
 
 <style scoped>
+.el-scrollbar {
+    display: inline-block;
+    width: 95%;
+}
 #motionrug-container {
     background-color: #fff;
 }
@@ -173,5 +212,21 @@ onMounted(() => {
 .slider-demo-block .el-slider {
     margin-top: 0;
     margin-left: 12px;
+}
+.slider-y-container {
+    display: inline;
+    align-items: center;
+    margin-left: 12px;
+    margin-right: 12px;
+    /* margin-bottom: 12px; */
+    /* line-height: 90px; */
+}
+.slider-y {
+    margin-top: 0;
+    padding-bottom: 12px;
+    /* margin-bottom: 12px; */
+}
+#canvas {
+    margin-top: 12px;
 }
 </style>
